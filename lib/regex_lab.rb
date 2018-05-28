f5785e2a6cc595c8c6d3a4c48a9e4edb35325153
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-28 17:09:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/^[A-Z][\s\S]+\.$/)
+  if text.match((/^[A-Z].*\W$/))
     true
   else
     false
